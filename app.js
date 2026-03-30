ce60dd3aaf2ebce403063b6aa121a21c7205ad33
KatAlmadanat <kathrinealmadanat@gmail.com>
2026-03-30 12:38:28 +0300

    C:/Program Files/Git/status url handling

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 //express is a web framework for building routes and  handling requests
 //require() loads moduels and run immediate logic once dep. are ready
 const express = require('express');
-const app =  express(); //server core, initializes new Express app
+const app =  express(); //server core, creates new Express app instance
 
+/*run json parser for each request made, provides middleware
+puts json content in req body*/ 
 app.use(express.json());
+app.get('/status', (req,res)) => {
+    res.json({
+        status: 'Running',
+        timestamp: new Data().toISOString()
+    })
+}
