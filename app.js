b09eefbb7d129cd9af18785807c8010ca9dcc96a
KatAlmadanat <kathrinealmadanat@gmail.com>
2026-03-31 13:21:16 +0300

    start the server

--- app.js
+++ app.js
@@ -6,9 +6,19 @@ const app =  express(); //server core, creates new Express app instance
 /*run json parser for each request made, provides middleware
 puts json content in req body*/ 
 app.use(express.json());
-app.get('/status', (req,res)) => {
+
+//defining routing for get requests with /status path
+app.get('/status', (req,res) => {
     res.json({
-        status: 'Running',
-        timestamp: new Data().toISOString()
-    })
-}
+        status: 'Running', //sets response status to 'running'
+        timestamp: new Data().toISOString()  //creates a timestamp with current time, converted to str
+    });
+});
+
+/*assign which port server is listening to
+if the process's environment port var is set, use that
+otherwise port 3000 (default)*/ 
+const PORT = process.env.PORT || 3000;
+
+//starting the server
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
